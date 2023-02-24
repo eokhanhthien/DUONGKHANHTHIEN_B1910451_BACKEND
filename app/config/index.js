05ab2025f5a6605b23ba23680d022b0ba18c0420
Thien <thienpark.fx@gmail.com>
2023-02-24 08:59:36 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,9 @@
 const config = {
     app: {
     port: process.env.PORT || 3000,
+    },
+    db:{
+        uri: process.env.MONGODB_URI || "mongodb+srv://thien:thien@cluster0.m28vgln.mongodb.net/test"
     }
     };
     module.exports = config;
